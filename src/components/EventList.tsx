afb55af512c635ecdf2011af8c7f9ffcc40e1e1b
Denis Eneotescu <deniseneotescu@gmail.com>
2025-01-07 18:34:41 +0200

    added event cards layout

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import Spinner from "@components/Spinner";
-import { CalendarDays } from "lucide-react";
+import { CalendarDays, Ticket } from "lucide-react";
 
 function EventList() {
   const events = useQuery(api.events.get);
@@ -18,6 +18,10 @@ function EventList() {
   const upcomingEvents = events
     ?.filter((event) => event.eventDate > Date.now())
     .sort((a, b) => a.eventDate - b.eventDate);
+  const pastEvents = events
+    ?.filter((event) => event.eventDate < Date.now())
+    .sort((a, b) => b.eventDate - a.eventDate);
+
   return (
     <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
       {/* Header */}
@@ -35,6 +39,35 @@ function EventList() {
           </div>
         </div>
       </div>
+
+      {/* Upcoming Events */}
+      {upcomingEvents.length > 0 ? (
+        <div className="mb-12 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {upcomingEvents.map((event) => (
+            <button key={event._id}>event card</button>
+          ))}
+        </div>
+      ) : (
+        <div className="mb-12 rounded-lg bg-gray-100 p-12 text-center">
+          <Ticket className="mx-auto h-12 w-12 text-gray-400" />
+          <h2 className="mb-1 mt-2 text-lg font-medium text-gray-900">
+            No upcoming events
+          </h2>
+          <p className="to-gray-600">Come back later for new events</p>
+        </div>
+      )}
+
+      {/* Past Events */}
+      {pastEvents.length > 0 && (
+        <>
+          <h2>Past Events</h2>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {pastEvents.map((event) => (
+              <button key={event._id}>event card</button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
